cce4145ef3820541c164aa9acfbda229fef90126
saasbook <saasbook@saasbook.(none)>
2015-10-29 05:03:34 -0300

    Initialized my project empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Denglu::Application.config.secret_token = 'eafd41f1bfa625151a7e3ea131202596f533ebcf74475f4e3f5d1ed40d0863af8b90b3b0dfeb05284b9a87378e2c95f785a1e9510d4aa6facbf53bd2906ebb07'
